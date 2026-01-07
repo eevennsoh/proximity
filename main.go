6eb0943be305427b6471131d3d178af0452289b3
vportella <vportella@atlassian.com>
2026-01-07 14:24:26 +1100

    update module in main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
 
-	"bitbucket.org/atlassian-developers/mini-proxy/internal/app"
+	"bitbucket.org/atlassian-developers/proximity/internal/app"
 )
 
 const (
